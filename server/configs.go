643cd5c58e74b7a97cb8de376547546798969e40
jasperchess <chesselet.jasper@gmail.com>
2026-01-03 22:16:47 +0200

    feedback applied

--- server/configs.go
+++ server/configs.go
@@ -1,5 +1,7 @@
 package server
 
+import "time"
+
 type WebhookConfig struct {
 	Url         string `usage:"If set, a POST request that contains connection status notifications will be sent to this HTTP address"`
 	RequireUser bool   `default:"false" usage:"Indicates if the webhook will only be called if a user is connecting rather than just server list/ping"`
@@ -24,31 +26,31 @@ type NgrokConfig struct {
 }
 
 type Config struct {
-	Port                          int               `default:"25565" usage:"The [port] bound to listen for Minecraft client connections"`
-	Default                       string            `usage:"host:port of a default Minecraft server to use when mapping not found"`
-	Mapping                       map[string]string `usage:"Comma or newline delimited or repeated mappings of externalHostname=host:port"`
-	ApiBinding                    string            `usage:"The [host:port] bound for servicing API requests"`
-	CpuProfile                    string            `usage:"Enables CPU profiling and writes to given path"`
-	ConnectionRateLimit           int               `default:"1" usage:"Max number of connections to allow per second"`
-	InKubeCluster                 bool              `usage:"Use in-cluster Kubernetes config"`
-	KubeConfig                    string            `usage:"The path to a Kubernetes configuration file"`
-	KubeNamespace                 string            `usage:"The namespace to watch or blank for all, which is the default"`
-	InDocker                      bool              `usage:"Use Docker service discovery"`
-	InDockerSwarm                 bool              `usage:"Use Docker Swarm service discovery"`
-	DockerSocket                  string            `default:"unix:///var/run/docker.sock" usage:"Path to Docker socket to use"`
-	DockerTimeout                 int               `default:"0" usage:"Timeout configuration in seconds for the Docker integrations"`
-	DockerRefreshInterval         int               `default:"15" usage:"Refresh interval in seconds for the Docker integrations"`
-	DockerApiVersion              string            `usage:"Instead of auto-negotiating, use specific Docker API version"`
-	MetricsBackend                string            `default:"discard" usage:"Backend to use for metrics exposure/publishing: discard,expvar,influxdb,prometheus"`
-	MetricsBackendConfig          MetricsBackendConfig
-	MetricsConnectionBucketPeriod int      `default:"1" usage:"The frequency (in seconds) at which the rate limit bucket's metrics are set: 0 to disable"`
-	UseProxyProtocol              bool     `default:"false" usage:"Send PROXY protocol to backend servers"`
-	ReceiveProxyProtocol          bool     `default:"false" usage:"Receive PROXY protocol from backend servers, by default trusts every proxy header that it receives, combine with -trusted-proxies to specify a list of trusted proxies"`
-	TrustedProxies                []string `usage:"Comma delimited list of CIDR notation IP blocks to trust when receiving PROXY protocol"`
-	RecordLogins                  bool     `default:"false" usage:"Log and generate metrics on player logins. Metrics only supported with influxdb or prometheus backend"`
-	Routes                        RoutesConfig
-	Ngrok                         NgrokConfig
-	AutoScale                     AutoScale
+	Port                   int               `default:"25565" usage:"The [port] bound to listen for Minecraft client connections"`
+	Default                string            `usage:"host:port of a default Minecraft server to use when mapping not found"`
+	Mapping                map[string]string `usage:"Comma or newline delimited or repeated mappings of externalHostname=host:port"`
+	ApiBinding             string            `usage:"The [host:port] bound for servicing API requests"`
+	CpuProfile             string            `usage:"Enables CPU profiling and writes to given path"`
+	ConnectionRateLimit    int               `default:"1" usage:"Max number of connections to allow per second"`
+	InKubeCluster          bool              `usage:"Use in-cluster Kubernetes config"`
+	KubeConfig             string            `usage:"The path to a Kubernetes configuration file"`
+	KubeNamespace          string            `usage:"The namespace to watch or blank for all, which is the default"`
+	InDocker               bool              `usage:"Use Docker service discovery"`
+	InDockerSwarm          bool              `usage:"Use Docker Swarm service discovery"`
+	DockerSocket           string            `default:"unix:///var/run/docker.sock" usage:"Path to Docker socket to use"`
+	DockerTimeout          int               `default:"0" usage:"Timeout configuration in seconds for the Docker integrations"`
+	DockerRefreshInterval  int               `default:"15" usage:"Refresh interval in seconds for the Docker integrations"`
+	DockerApiVersion       string            `usage:"Instead of auto-negotiating, use specific Docker API version"`
+	MetricsBackend         string            `default:"discard" usage:"Backend to use for metrics exposure/publishing: discard,expvar,influxdb,prometheus"`
+	MetricsBackendConfig   MetricsBackendConfig
+	MetricsRateLimitPeriod time.Duration `default:"1s" usage:"The period at which the rate limit bucket's metrics are set: 0 to disable (default 1s)"`
+	UseProxyProtocol       bool          `default:"false" usage:"Send PROXY protocol to backend servers"`
+	ReceiveProxyProtocol   bool          `default:"false" usage:"Receive PROXY protocol from backend servers, by default trusts every proxy header that it receives, combine with -trusted-proxies to specify a list of trusted proxies"`
+	TrustedProxies         []string      `usage:"Comma delimited list of CIDR notation IP blocks to trust when receiving PROXY protocol"`
+	RecordLogins           bool          `default:"false" usage:"Log and generate metrics on player logins. Metrics only supported with influxdb or prometheus backend"`
+	Routes                 RoutesConfig
+	Ngrok                  NgrokConfig
+	AutoScale              AutoScale
 
 	ClientsToAllow []string `usage:"Zero or more client IP addresses or CIDRs to allow. Takes precedence over deny."`
 	ClientsToDeny  []string `usage:"Zero or more client IP addresses or CIDRs to deny. Ignored if any configured to allow"`
--- server/connector.go
+++ server/connector.go
@@ -111,13 +111,13 @@ func (c *Connector) UseClientFilter(filter *ClientFilter) {
 	c.clientFilter = filter
 }
 
-func (c *Connector) StartAcceptingConnections(listenAddress string, connRateLimit, metricsFrequency int) error {
+func (c *Connector) StartAcceptingConnections(listenAddress string, connRateLimit int, metricsPeriod time.Duration) error {
 	ln, err := c.createListener(listenAddress)
 	if err != nil {
 		return err
 	}
 
-	go c.acceptConnections(ln, connRateLimit, metricsFrequency)
+	go c.acceptConnections(ln, connRateLimit, metricsPeriod)
 
 	return nil
 }
@@ -201,13 +201,13 @@ func (c *Connector) AcceptConnection(conn net.Conn) {
 	go c.HandleConnection(conn)
 }
 
-func (c *Connector) acceptConnections(ln net.Listener, connRateLimit, metricsFrequency int) {
+func (c *Connector) acceptConnections(ln net.Listener, connRateLimit int, metricsPeriod time.Duration) {
 	//noinspection GoUnhandledErrorResult
 	defer ln.Close()
 
 	bucket := ratelimit.NewBucketWithRate(float64(connRateLimit), int64(connRateLimit*2))
-	if metricsFrequency > 0 {
-		go c.bucketMetrics(bucket, metricsFrequency)
+	if metricsPeriod > 0 {
+		go c.bucketMetrics(bucket, metricsPeriod)
 	}
 
 	for {
@@ -226,8 +226,8 @@ func (c *Connector) acceptConnections(ln net.Listener, connRateLimit, metricsFre
 	}
 }
 
-func (c *Connector) bucketMetrics(bucket *ratelimit.Bucket, frequency int) {
-	ticker := time.NewTicker(time.Duration(frequency) * time.Second)
+func (c *Connector) bucketMetrics(bucket *ratelimit.Bucket, period time.Duration) {
+	ticker := time.NewTicker(period)
 	defer ticker.Stop()
 	for {
 		select {
